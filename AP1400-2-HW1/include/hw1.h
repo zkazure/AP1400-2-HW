af8440dd945836af7c4463d7c7a527595f301dbe
Kazure Zheng <kazure@debian>
2025-09-02 20:33:05 +0800

    finish create matrix

--- AP1400-2-HW1/include/hw1.h
+++ AP1400-2-HW1/include/hw1.h
@@ -1,5 +1,17 @@
 #ifndef AP_HW1_H
 #define AP_HW1_H
 
+#include <vector>
+using std::size_t;
+
+namespace algebra
+{
+    using Matrix = std::vector<std::vector<double>>;
+    
+    Matrix zeros(size_t n, size_t m);
+    Matrix ones(size_t n, size_t m);
+    Matrix random(size_t n, size_t m, double min, double max);
+
+}
 
 #endif //AP_HW1_H
--- AP1400-2-HW1/src/hw1.cpp
+++ AP1400-2-HW1/src/hw1.cpp
@@ -1,1 +1,43 @@
 #include "hw1.h"
+#include <iostream>
+#include <random>
+
+namespace algebra
+{
+    // Create martix
+    Matrix create_matrix(size_t n, size_t m, double v) {
+        if (n<=0 || m<=0)
+            throw std::logic_error("invalid matrix\n");
+        Matrix x;
+        for (int i=0; i<n; ++i) {
+            std::vector<double> vect(m, v);
+            x.push_back(vect);
+        }
+        return x;
+    }
+    
+    Matrix zeros(size_t n, size_t m) { return create_matrix(n, m, 0); }
+
+    Matrix ones(size_t n, size_t m) { return create_matrix(n, m, 1); }
+
+    Matrix random(size_t n, size_t m, double min, double max) {
+        if (min > max)
+            throw std::logic_error("min can't not greater than max!\n");
+
+        auto x = create_matrix(n, m, min);
+        if (min!=max) {
+            std::random_device rd;
+            std::mt19937 gen(rd());
+            std::uniform_real_distribution<double> dis(min, max);
+
+            for (int i=0; i<n; ++i)
+                for (int j=0; j<m; ++j)
+                    x[i][j] = dis(gen);
+        }
+
+        return x;        
+    }
+
+}
+
+
